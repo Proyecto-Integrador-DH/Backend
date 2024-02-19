b5e16c08f90bf629abc5b048737ced21ca63467f
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-02-19 15:39:59 -0500

    Solucionado: guardar imágenes en carpeta en s3 y url en bd

--- src/main/java/com/example/ProyectoIntegradorBack/Service/ImagenCarga.java
+++ src/main/java/com/example/ProyectoIntegradorBack/Service/ImagenCarga.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.ProyectoIntegradorBack.Model.Imagen;
-import com.example.ProyectoIntegradorBack.Model.ImagenDTO;
 import com.example.ProyectoIntegradorBack.Repository.IImagenRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -31,12 +30,15 @@ public class ImagenCarga {
     private final String bucketName = "test-s3-java-upload";
 
     public void cargarImagen(Imagen imagen){
+
         List<String> base64Images = imagen.getImgPath() != null ? imagen.getImgPath() : Collections.emptyList();
         String altText = imagen.getAltText();
+        RandomLetras randomLetras = new RandomLetras();
+        String path = randomLetras.randomString(4);
 
         for(int i = 0; i < base64Images.size(); i++){
             String base64 = base64Images.get(i);
-            String altTextImg = altText + "img" != null ? altText + i : "Imagen " + i;
+            String keyName = path + "/" + altText+ i + ".png";
 
             byte[] decodedBytes = Base64.getDecoder().decode(base64);
 
@@ -47,11 +49,27 @@ public class ImagenCarga {
                 e.printStackTrace();
             }
 
-            imagenRepository.save(imagen);
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, altTextImg, file);
+            Imagen imagenNueva = new Imagen();
+            imagenNueva.setAltText(altText);
+            imagenNueva.setUrl("https://test-s3-java-upload.s3.amazonaws.com/" + keyName);
+            imagenRepository.save(imagenNueva);
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, keyName, file);
             s3.putObject(putObjectRequest);
 
             System.out.println("Imagen cargada exitosamente a S3.");
+        }
+    }
+
+    public class RandomLetras {
+        public String randomString(int length) {
+            String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                int index = (int)(letras.length() * Math.random());
+                sb.append(letras.charAt(index));
+            }
+            return sb.toString();
         }
     }
 }
