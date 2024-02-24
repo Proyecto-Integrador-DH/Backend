a2d0f87796d066ec825cd070a37b28ecfea1210b
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-02-24 17:20:15 -0500

    Configuración de acceso CORS

--- src/main/java/com/example/ProyectoIntegradorBack/ProyectoIntegradorBackApplication.java
+++ src/main/java/com/example/ProyectoIntegradorBack/ProyectoIntegradorBackApplication.java
@@ -2,11 +2,13 @@ package com.example.ProyectoIntegradorBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProyectoIntegradorBackApplication {
 
 	public static void main(String[] args) {
