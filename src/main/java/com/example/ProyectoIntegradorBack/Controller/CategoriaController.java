e6060f937cf733c937d718c9e7c143da6985b1b7
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-03-07 15:58:25 -0500

    Corrección

--- src/main/java/com/example/ProyectoIntegradorBack/Controller/CategoriaController.java
+++ src/main/java/com/example/ProyectoIntegradorBack/Controller/CategoriaController.java
@@ -22,7 +22,7 @@ public class CategoriaController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getCategoria(@PathVariable Integer id){
         try {
-            CategoriaNombreDTO categoriaDTO = categoriaService.getCategoria(id);it a
+            CategoriaNombreDTO categoriaDTO = categoriaService.getCategoria(id);
             if (categoriaDTO != null) {
                 return ResponseEntity.ok(categoriaDTO);
             } else {
